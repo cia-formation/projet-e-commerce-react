c7dde051e1e65b0ce4e313bd4f9bc627980d1cac
EssiJunior <nessipjunior@gmail.com>
2025-04-23 12:04:40 +0100

    Events and useState applied on Sign In page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -7,9 +7,29 @@ import Typography from "../../components/Typography/Typography"
 
 import './signin.css'
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 
 const SignIn = () => {
     const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = (event) => {
+        setEmail(event.target.value)
+    }
+    const handlePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('Email : '+email)
+        console.log('Mot de passe : '+password)
+
+        navigate('/enregistrement-produit')
+    }
 
     return (
         <div className='signin '>
@@ -17,9 +37,19 @@ const SignIn = () => {
                 <Heading label={'MA BOUTIQUE'} />
                 <Typography text={'CONNEXION'} fontWeight={600} fontSize={'22px'} color={'black'} />
                 <div className="signin-form">
-                    <InputText label="Email" placeholder="Email" name="email" type="email" />
-                    <InputText label="Mot de passe" placeholder="Mot de passe" name="password" type="password" />
-                    <Button label={'SIGN IN'} width={'415px'} handleClick={() => navigate('/enregistrement-produit')}/>
+                    <InputText
+                        label="Email"
+                        placeholder="Email"
+                        name="email"
+                        type="email"
+                        setValue={handleEmail} />
+                    <InputText
+                        label="Mot de passe"
+                        placeholder="Mot de passe"
+                        name="password"
+                        type="password"
+                        setValue={handlePassword}  />
+                    <Button label={'SIGN IN'} width={'415px'} handleClick={handleSubmit} />
                 </div>
             </div>
         </div>
